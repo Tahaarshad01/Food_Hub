540a75e8ca732e8b2d1c0875261b8c766a6dc3ba
Tahaarshad01 <ta.arshad786@gmail.com>
2024-05-03 03:12:05 +0530

    allow cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import path from "path";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/", routes);
